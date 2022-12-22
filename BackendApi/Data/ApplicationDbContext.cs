1f837791fb4cdbb86a9e700e7f98b409f68c1e94
Rupert Navarro <rupertmarty.navarro.954@my.csun.edu>
2022-12-22 03:25:20 -0800

    project complete

---
+++ BackendApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BackendApi.Data.Models;
+using System.Diagnostics.Metrics;
+
+namespace BackendApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+        public DbSet<Address> Addresses => Set<Address>();
+    }
+}
